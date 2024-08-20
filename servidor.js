0e4e1540a17d607aa4a134881f4b05e841f9ac67
Saymon Mello de Almeida <20211PF.INF_I0008@passofundo.ifsul.local>
2024-08-20 10:10:57 -0300

    mensagem

--- servidor.js
+++ servidor.js
@@ -160,6 +160,91 @@ sw.get('/listjogadores', function (req, res, next) {
 
 
 
+
+
+
+
+
+
+
+
+sw.post('/insertpatente', function (req, res, next) {
+    
+    postgres.connect(function(err,client,done) {
+
+       if(err){
+
+           console.log("Nao conseguiu acessar o  BD "+ err);
+           res.status(400).send('{'+err+'}');
+       }else{            
+
+            var q1 ={
+                text: 'insert into tb_patente (nome, quant_min_pontos, datacriacao, cor, datacadastro, ' +
+                   ' situacao) ' +
+                ' values ($1,$2,$3,$4,now(), $5) ' +
+                                            'returning nome, quant_min_pontos, datacriacao, cor, ' +
+                                            ' to_char(datacadastro, \'dd/mm/yyyy\') as datacadastro, '+
+                                            ' to_char(data_ultimo_login, \'dd/mm/yyyy\') as data_ultimo_login, situacao;',
+                values: [req.body.nome, 
+                         req.body.quant_min_pontos, 
+                         req.body.datacriacao, 
+                         req.body.cor, 
+                         req.body.situacao == true ? "A" : "I"]
+            }
+            var q2 = {
+                text : 'insert into tb_endereco (complemento, cep, nomejogador) values ($1, $2, $3) returning codigo, complemento, cep;',
+                values: [req.body.endereco.complemento, 
+                         req.body.endereco.cep, 
+                         req.body.nome]
+            }
+            console.log(q1);
+
+            client.query(q1, function(err,result1) {
+                if(err){
+                    console.log('retornou 400 no insert q1');
+                    res.status(400).send('{'+err+'}');
+                }else{
+                    client.query(q2, async function(err,result2) {
+                        if(err){
+                            console.log('retornou 400 no insert q2');
+                            res.status(400).send('{'+err+'}');
+                        }else{
+                        
+                            //insere todas as pantentes na tabela associativa.
+                            for(var i=0; i < req.body.patentes.length; i++){                                              
+
+                                try {                          
+        
+                                    await client.query('insert into tb_jogador_conquista_patente (codpatente, nome) values ($1, $2)', [req.body.patentes[i].codigo, req.body.nome])
+        
+                                } catch (err) {
+                                                                
+                                    res.status(400).send('{'+err+'}');
+                                }                                           
+        
+                            }                            
+
+                            done(); // closing the connection;
+                            console.log('retornou 201 no insertjogador');
+                            res.status(201).send({"nome" : result1.rows[0].nome, 
+                                                  "quant_min_pontos": result1.rows[0].quant_min_pontos, 
+                                                  "datacriacao": result1.rows[0].datacriacao, 
+                                                  "cor": result1.rows[0].cor,
+                                                  "situacao": result1.rows[0].situacao,
+                                                  "datacadastro" : result1.rows[0].datacadastro,
+                                                  "data_ultimo_login" : result1.rows[0].data_ultimo_login,
+                                                  "endereco": {"codigo": result2.rows[0].codigo, "cep": result2.rows[0].cep, "complemento": result2.rows[0].complemento},
+                                                  "patentes": req.body.patentes});
+                        }
+                    });
+                }           
+            });
+       }       
+    });
+});
+
+
+
 sw.listen(4000, function () {
     console.log('Servidor ta bombando na porta 4milão');
 });
